761df07a5fa19d67f1452f247c15ec5eff943c32
rosicky_tong <rosicky_tong@foxmail.com>
2024-04-22 16:43:01 +0800

    update

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
 cd CloudflareSpeedTest
-.\CloudflareSpeedTest.exe -url "http://lg-sj.racknerd.com/100MB.test" -tp 80 -tl 200 -tll 30 -sl 5
+.\CloudflareSpeedTest.exe -url "http://lg-sj.racknerd.com/25MB.test" -tp 80 -tl 200 -tll 30 -sl 5
 
